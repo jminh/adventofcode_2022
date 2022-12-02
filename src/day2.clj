4418a045049964800aab5fe59d2bf29790fe738e
ming <ming>
2022-12-02 13:40:23 +0800

    day2 part2 solution

--- src/day2.clj
+++ src/day2.clj
@@ -34,3 +34,23 @@ C Z
        (apply +)))
 
 (prn ans1)
+
+(def score-map {["A", "X"] (+ 0 3)
+                ["A", "Y"] (+ 3 1)
+                ["A", "Z"] (+ 6 2)
+                ["B", "X"] (+ 0 1)
+                ["B", "Y"] (+ 3 2)
+                ["B", "Z"] (+ 6 3)
+                ["C", "X"] (+ 0 2)
+                ["C", "Y"] (+ 3 3)
+                ["C", "Z"] (+ 6 1)})
+
+(def ans2
+  (->> "src/input/day2.txt"
+       slurp
+       str/split-lines
+       (map #(str/split % #" "))
+       (map score-map)
+       (apply +)))
+
+(prn ans2)
